608bd64d16daa22b602d9812568e8cb0e0a29e3f
MP <mm.potapov@mail.ru>
2023-02-10 01:36:27 +0300

    ref: new structure of header

--- src/components/Header.js
+++ src/components/Header.js
@@ -14,20 +14,25 @@ function Header() {
     };
   }
 
+
   return (
     <header className="header">
+      <div className={`header__mobile ${isAccountInfoOpen && "header__mobile_active"}`}>
+        <p className="header__email">email@mail.com</p>
+        <button className="header__button-profile">Выйти</button>
+      </div>
       <div className="header__main">
         <a href="#0" className="header__logo-link">
           <img alt="Логотип" src={logo} className="header__logo" />
         </a>
-        <button
-          onClick={handleButtonHideClick}
-          className={`header__button-hide ${isAccountInfoOpen && "header__button-hide_active"}`}>
-        </button>
-      </div>
-      <div className={`header__account ${isAccountInfoOpen && "header__account_active"}`}>
-        <p className="header__email">email@mail.com</p>
-        <button className="header__button-exit">Выйти</button>
+        <div className={`header__account`}>
+          <button
+            onClick={handleButtonHideClick}
+            className={`header__button-hide ${isAccountInfoOpen && "header__button-hide_active"}`}>
+          </button>
+          <p className="header__email">email@mail.com</p>
+          <button className="header__button-profile">Выйти</button>
+        </div>
       </div>
     </header>
   )
